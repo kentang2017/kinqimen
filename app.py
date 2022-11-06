7676118f16da57ae1c8c3187423a4bff26aeb0de
Ken Tang <34528743+kentang2017@users.noreply.github.com>
2022-11-06 12:47:45 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,31 @@
+import streamlit as st
+import pendulum as pdlm
+from contextlib import contextmanager, redirect_stdout
+import streamlit.components.v1 as components
+from kintaiyi import Taiyi
 
+
+@contextmanager
+def st_capture(output_func):
+    with StringIO() as stdout, redirect_stdout(stdout):
+        old_write = stdout.write
+        def new_write(string):
+            ret = old_write(string)
+            output_func(stdout.getvalue())
+            return ret
+        stdout.write = new_write
+        yield
+        
+st.set_page_config(layout="wide",page_title="堅奇門 - 奇門遁甲排盘")
+pan,example,guji,update = st.tabs([' 排盤 ', ' 案例 ', ' 古籍 ',' 日誌 ' ])
+with st.sidebar:
+    pp_date=st.date_input("日期",pdlm.now(tz='Asia/Shanghai').date())
+    pp_time=st.time_input("時間",pdlm.now(tz='Asia/Shanghai').time())
+    p = str(pp_date).split("-")
+    pp = str(pp_time).split(":")
+    y = int(p[0])
+    m = int(p[1])
+    d = int(p[2])
+    h = int(pp[0])
+    min = int(pp[1])
+    
