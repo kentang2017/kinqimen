90212882591920c211d7d0386d6dad4823be6bb8
Ken Tang <34528743+kentang2017@users.noreply.github.com>
2022-11-06 13:02:00 +0800

    Update app.py

--- app.py
+++ app.py
@@ -29,7 +29,7 @@ with st.sidebar:
     min = int(pp[1])
     
 with pan:
-    qtext = Qimen(y,m,d,h).pan()
+    qtext = kinqimen.Qimen(y,m,d,h).pan()
     eg = list("巽離坤震兌艮坎乾")
     qd = [qtext.get("地盤").get(i) for i in eg]
     try:
