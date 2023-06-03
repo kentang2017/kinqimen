22640618e57a91ec7bd493d60da28aefbe0702ab
Ken Tang <34528743+kentang2017@users.noreply.github.com>
2023-06-03 22:46:29 +0800

    Update app.py

--- app.py
+++ app.py
@@ -20,6 +20,11 @@ def st_capture(output_func):
         yield
         
 def get_file_content_as_string(path):
+    url = 'https://raw.githubusercontent.com/kentang2017/kinqimen/master/' + path
+    response = urllib.request.urlopen(url)
+    return response.read().decode("utf-8")
+
+def get_file_content_as_string1(path):
     url = 'https://raw.githubusercontent.com/kentang2017/kinliuren/master/' + path
     response = urllib.request.urlopen(url)
     return response.read().decode("utf-8")
@@ -39,7 +44,7 @@ with st.sidebar:
    
 with update:
     st.header('日誌')
-    st.markdown(get_file_content_as_string("update.md"))
+    st.markdown(get_file_content_as_string1("update.md"))
     
 with pan:
     st.header('奇門穿壬')
