7f0541ec94d2b9bf775ac86ea129ee7a0a77042c
Ken Tang <34528743+kentang2017@users.noreply.github.com>
2024-06-04 21:16:23 +0800

    Update config.py

--- config.py
+++ config.py
@@ -377,6 +377,12 @@ def qimen_ju_name_zhirun(year, month, day, hour, minute):
         find_yingyang = multi_key_dict_get(yy,new_jq)
         return "{}{}局{}".format(find_yingyang, kooks, three_yuen)
     if dgz_dist == "日干非符頭" and difference == 15: #接氣
+        new_jq = new_list(jieqi_name, Jieqi)[1]
+        jieqi_code = jieqicode_jq(new_jq)
+        kooks =  {"上元":jieqi_code[0],
+                  "中元":jieqi_code[1],
+                  "下元":jieqi_code[2]}.get(three_yuen)
+        find_yingyang = multi_key_dict_get(yy,new_jq)
         return "{}{}局{}".format(find_yingyang, kooks, three_yuen)
     if dgz_dist == "日干是符頭" and difference == 0 :#正授
         return "{}{}局{}".format(find_yingyang, kooks, three_yuen)
@@ -741,4 +747,4 @@ def jq_distance(year, month, day, hour, minute):
 
 
 if __name__ == '__main__':
-    print(qimen_ju_name_zhirun(2024, 6, 29, 12, 30))
+    print(qimen_ju_name_zhirun(2024, 6, 5, 1, 0))
