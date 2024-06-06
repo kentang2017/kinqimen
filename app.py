270f301fa3ed05d4e20f5aafd340b90aa8bcc270
Ken Tang <34528743+kentang2017@users.noreply.github.com>
2024-06-06 22:24:01 +0800

    Update app.py

--- app.py
+++ app.py
@@ -61,32 +61,94 @@ with log:
 
 with pan:
     st.header('堅奇門')
-    output4 = st.empty()
-    with st_capture(output4.code):
-        now = datetime.datetime.now(pytz.timezone('Asia/Hong_Kong'))
-        ny = now.year
-        nm = now.month
-        nd = now.day
-        nh = now.hour
-        nmintue = now.minute
+    now = datetime.datetime.now(pytz.timezone('Asia/Hong_Kong'))
+    ny = now.year
+    nm = now.month
+    nd = now.day
+    nh = now.hour
+    nmintue = now.minute
+    if not manual:
+        j_q =  config.jq(ny,nm,nd,nh,nmintue)
+        gz = config.gangzhi(ny,nm,nd,nh,nmintue)
+        qtext = kinqimen.Qimen(ny,nm,nd,nh,nmintue).pan(pai)
+        lunar_month = dict(zip(range(1,13), config.cmonth)).get(config.lunar_date_d(ny,nm,nd).get("月"))
+        lr = kinliuren.Liuren( qtext.get("節氣"),lunar_month, gz[2], gz[3]).result(0)
+        qd = [qtext.get("地盤").get(i) for i in eg]
+        e_to_s = lr.get("地轉天盤")
+        e_to_g = lr.get("地轉天將")
+        qt = [qtext.get('天盤', {}).get(i) for i in eg]
+        god = [qtext.get("神").get(i) for i in eg]
+        door = [qtext.get("門").get(i) for i in eg]
+        star = [qtext.get("星").get(i) for i in eg]
+        md = qtext.get("地盤").get("中")
+        print("時家奇門 | {}".format(qtext.get("排盤方式")))
+        print("{}年{}月{}日{}時\n".format(y,m,d,h))
+        print("{} |\n{} | 節氣︰{} |\n值符星宮︰天{}宮 | 值使門宮︰{}\n".format(qtext.get("干支"), qtext.get("排局"),  j_q,  qtext.get("值符值使").get("值符星宮")[0]+"-"+qtext.get("值符值使").get("值符星宮")[1], qtext.get("值符值使").get("值使門宮")[0]+"門"+qtext.get("值符值使").get("值使門宮")[1]+"宮" ))
+        print("＼  {}{}  　 │  {}{}　 │  {}{}　 │  　 {}{}　 ／".format(e_to_s.get("巳"),e_to_g.get("巳"),e_to_s.get("午"),e_to_g.get("午"),e_to_s.get("未"),e_to_g.get("未"),e_to_s.get("申"),e_to_g.get("申")))
+        print("  ＼────────┴──┬─────┴─────┬──┴────────／")
+        print(" 　│　　{}　　　 │　　{}　　　 │　　{}　　　 │".format(god[0], god[1], god[2]))
+        print(" 　│　　{}　　{} │　　{}　　{} │　　{}　　{} │".format(door[0], qt[0], door[1], qt[1], door[2], qt[2]))
+        print(" 　│　　{}　　{} │　　{}　　{} │　　{}　　{} │".format(star[0], qd[0], star[1], qd[1], star[2], qd[2]))
+        print(" {}├───────────┼───────────┼───────────┤{}".format(e_to_s.get("辰"),e_to_s.get("酉")))
+        print(" {}│　　{}　　　 │　　　　　　 │　　{}　　　 │{}".format(e_to_g.get("辰"),god[3], god[4],e_to_g.get("酉")))
+        print("　─┤　　{}　　{} │　　　　　　 │　　{}　　{} ├─".format(door[3], qt[3],  door[4], qt[4]))
+        print(" 　│　　{}　　{} │　　　　　{} │　　{}　　{} │".format(star[3], qd[3], md, star[4], qd[4]))
+        print(" 　├───────────┼───────────┼───────────┤")
+        print("　 │　　{}　　　 │　　{}　　　 │　　{}　　　 │".format(god[5], god[6], god[7]))
+        print(" {}│　　{}　　{} │　　{}　　{} │　　{}　　{} │{}".format(e_to_s.get("卯"),door[5], qt[5], door[6], qt[6], door[7], qt[7], e_to_s.get("戌")))
+        print(" {}│　　{}　　{} │　　{}　　{} │　　{}　　{} │{}".format(e_to_g.get("卯"),star[5], qd[5], star[6], qd[6], star[7], qd[7], e_to_g.get("戌")))
+        print("  ／────────┬──┴─────┬─────┴──┬────────＼")
+        print("／  {}{}  　 │  {}{}　 │  {}{}　 │  　 {}{}　 ＼".format(e_to_s.get("寅"),e_to_g.get("寅"),e_to_s.get("丑"),e_to_g.get("丑"),e_to_s.get("子"),e_to_g.get("子"),e_to_s.get("亥"),e_to_g.get("亥")))
+        expander = st.expander("原始碼")
+        expander.write(str(qtext))
+    if manual:
         gz = config.gangzhi(y,m,d,h,mintue)
         j_q =  config.jq(y, m, d, h, mintue)
         eg = list("巽離坤震兌艮坎乾")
-        if not manual:
-            j_q =  config.jq(ny,nm,nd,nh,nmintue)
-            gz = config.gangzhi(ny,nm,nd,nh,nmintue)
-            qtext = kinqimen.Qimen(ny,nm,nd,nh,nmintue).pan(pai)
-            lunar_month = dict(zip(range(1,13), config.cmonth)).get(config.lunar_date_d(ny,nm,nd).get("月"))
+        lunar_month = dict(zip(range(1,13), config.cmonth)).get(config.lunar_date_d(y,m,d).get("月"))
+        if num == 1:
+            qtext = kinqimen.Qimen(y,m,d,h,mintue).pan(pai)
             lr = kinliuren.Liuren( qtext.get("節氣"),lunar_month, gz[2], gz[3]).result(0)
             qd = [qtext.get("地盤").get(i) for i in eg]
             e_to_s = lr.get("地轉天盤")
             e_to_g = lr.get("地轉天將")
-            qt = [qtext.get('天盤', {}).get(i) for i in eg]
+            qt = [qtext['天盤'].get(i) for i in eg]
             god = [qtext.get("神").get(i) for i in eg]
             door = [qtext.get("門").get(i) for i in eg]
             star = [qtext.get("星").get(i) for i in eg]
             md = qtext.get("地盤").get("中")
             print("時家奇門 | {}".format(qtext.get("排盤方式")))
+            print("{}年{}月{}日{}時\n".format(y,m,d,h))
+            print("{} |\n{} | 節氣︰{} |\n值符天干︰{} |\n值符星宮︰天{}宮 | 值使門宮︰{}\n".format(qtext.get("干支"), qtext.get("排局"),  j_q, qtext.get("值符值使").get("值符天干")[0]+qtext.get("值符值使").get("值符天干")[1],  qtext.get("值符值使").get("值符星宮")[0]+"-"+qtext.get("值符值使").get("值符星宮")[1], qtext.get("值符值使").get("值使門宮")[0]+"門"+qtext.get("值符值使").get("值使門宮")[1]+"宮" ))
+            print("＼  {}{}  　 │  {}{}　 │  {}{}　 │  　 {}{}　 ／".format(e_to_s.get("巳"),e_to_g.get("巳"),e_to_s.get("午"),e_to_g.get("午"),e_to_s.get("未"),e_to_g.get("未"),e_to_s.get("申"),e_to_g.get("申")))
+            print("  ＼────────┴──┬─────┴─────┬──┴────────／")
+            print(" 　│　　{}　　　 │　　{}　　　 │　　{}　　　 │".format(god[0], god[1], god[2]))
+            print(" 　│　　{}　　{} │　　{}　　{} │　　{}　　{} │".format(door[0], qt[0], door[1], qt[1], door[2], qt[2]))
+            print(" 　│　　{}　　{} │　　{}　　{} │　　{}　　{} │".format(star[0], qd[0], star[1], qd[1], star[2], qd[2]))
+            print(" {}├───────────┼───────────┼───────────┤{}".format(e_to_s.get("辰"),e_to_s.get("酉")))
+            print(" {}│　　{}　　　 │　　　　　　 │　　{}　　　 │{}".format(e_to_g.get("辰"),god[3], god[4],e_to_g.get("酉")))
+            print("　─┤　　{}　　{} │　　　　　　 │　　{}　　{} ├─".format(door[3], qt[3],  door[4], qt[4]))
+            print(" 　│　　{}　　{} │　　　　　{} │　　{}　　{} │".format(star[3], qd[3], md, star[4], qd[4]))
+            print(" 　├───────────┼───────────┼───────────┤")
+            print("　 │　　{}　　　 │　　{}　　　 │　　{}　　　 │".format(god[5], god[6], god[7]))
+            print(" {}│　　{}　　{} │　　{}　　{} │　　{}　　{} │{}".format(e_to_s.get("卯"),door[5], qt[5], door[6], qt[6], door[7], qt[7], e_to_s.get("戌")))
+            print(" {}│　　{}　　{} │　　{}　　{} │　　{}　　{} │{}".format(e_to_g.get("卯"),star[5], qd[5], star[6], qd[6], star[7], qd[7], e_to_g.get("戌")))
+            print("  ／────────┬──┴─────┬─────┴──┬────────＼")
+            print("／  {}{}  　 │  {}{}　 │  {}{}　 │  　 {}{}　 ＼".format(e_to_s.get("寅"),e_to_g.get("寅"),e_to_s.get("丑"),e_to_g.get("丑"),e_to_s.get("子"),e_to_g.get("子"),e_to_s.get("亥"),e_to_g.get("亥")))
+            expander = st.expander("原始碼")
+            expander.write(str(qtext))
+        if num == 2:
+            qtext = kinqimen.Qimen(y,m,d,h,mintue).pan_minute(pai)
+            lr = kinliuren.Liuren( qtext.get("節氣"),lunar_month, gz[3], gz[4]).result(0)
+            qd = [qtext.get("地盤").get(i) for i in eg]
+            e_to_s = lr.get("地轉天盤")
+            e_to_g = lr.get("地轉天將")
+            qt = [qtext.get("天盤").get(i) for i in eg]
+            god = [qtext.get("神").get(i) for i in eg]
+            door = [qtext.get("門").get(i) for i in eg]
+            star = [qtext.get("星").get(i) for i in eg]
+            md = qtext.get("地盤").get("中")
+            print("刻家奇門 | {}".format(qtext.get("排盤方式")))
             print("{}年{}月{}日{}時\n".format(y,m,d,h))
             print("{} |\n{} | 節氣︰{} |\n值符星宮︰天{}宮 | 值使門宮︰{}\n".format(qtext.get("干支"), qtext.get("排局"),  j_q,  qtext.get("值符值使").get("值符星宮")[0]+"-"+qtext.get("值符值使").get("值符星宮")[1], qtext.get("值符值使").get("值使門宮")[0]+"門"+qtext.get("值符值使").get("值使門宮")[1]+"宮" ))
             print("＼  {}{}  　 │  {}{}　 │  {}{}　 │  　 {}{}　 ／".format(e_to_s.get("巳"),e_to_g.get("巳"),e_to_s.get("午"),e_to_g.get("午"),e_to_s.get("未"),e_to_g.get("未"),e_to_s.get("申"),e_to_g.get("申")))
@@ -106,70 +168,3 @@ with pan:
             print("／  {}{}  　 │  {}{}　 │  {}{}　 │  　 {}{}　 ＼".format(e_to_s.get("寅"),e_to_g.get("寅"),e_to_s.get("丑"),e_to_g.get("丑"),e_to_s.get("子"),e_to_g.get("子"),e_to_s.get("亥"),e_to_g.get("亥")))
             expander = st.expander("原始碼")
             expander.write(str(qtext))
-        if manual:
-            gz = config.gangzhi(y,m,d,h,mintue)
-            j_q =  config.jq(y, m, d, h, mintue)
-            eg = list("巽離坤震兌艮坎乾")
-            lunar_month = dict(zip(range(1,13), config.cmonth)).get(config.lunar_date_d(y,m,d).get("月"))
-            if num == 1:
-                qtext = kinqimen.Qimen(y,m,d,h,mintue).pan(pai)
-                lr = kinliuren.Liuren( qtext.get("節氣"),lunar_month, gz[2], gz[3]).result(0)
-                qd = [qtext.get("地盤").get(i) for i in eg]
-                e_to_s = lr.get("地轉天盤")
-                e_to_g = lr.get("地轉天將")
-                qt = [qtext['天盤'].get(i) for i in eg]
-                god = [qtext.get("神").get(i) for i in eg]
-                door = [qtext.get("門").get(i) for i in eg]
-                star = [qtext.get("星").get(i) for i in eg]
-                md = qtext.get("地盤").get("中")
-                print("時家奇門 | {}".format(qtext.get("排盤方式")))
-                print("{}年{}月{}日{}時\n".format(y,m,d,h))
-                print("{} |\n{} | 節氣︰{} |\n值符天干︰{} |\n值符星宮︰天{}宮 | 值使門宮︰{}\n".format(qtext.get("干支"), qtext.get("排局"),  j_q, qtext.get("值符值使").get("值符天干")[0]+qtext.get("值符值使").get("值符天干")[1],  qtext.get("值符值使").get("值符星宮")[0]+"-"+qtext.get("值符值使").get("值符星宮")[1], qtext.get("值符值使").get("值使門宮")[0]+"門"+qtext.get("值符值使").get("值使門宮")[1]+"宮" ))
-                print("＼  {}{}  　 │  {}{}　 │  {}{}　 │  　 {}{}　 ／".format(e_to_s.get("巳"),e_to_g.get("巳"),e_to_s.get("午"),e_to_g.get("午"),e_to_s.get("未"),e_to_g.get("未"),e_to_s.get("申"),e_to_g.get("申")))
-                print("  ＼────────┴──┬─────┴─────┬──┴────────／")
-                print(" 　│　　{}　　　 │　　{}　　　 │　　{}　　　 │".format(god[0], god[1], god[2]))
-                print(" 　│　　{}　　{} │　　{}　　{} │　　{}　　{} │".format(door[0], qt[0], door[1], qt[1], door[2], qt[2]))
-                print(" 　│　　{}　　{} │　　{}　　{} │　　{}　　{} │".format(star[0], qd[0], star[1], qd[1], star[2], qd[2]))
-                print(" {}├───────────┼───────────┼───────────┤{}".format(e_to_s.get("辰"),e_to_s.get("酉")))
-                print(" {}│　　{}　　　 │　　　　　　 │　　{}　　　 │{}".format(e_to_g.get("辰"),god[3], god[4],e_to_g.get("酉")))
-                print("　─┤　　{}　　{} │　　　　　　 │　　{}　　{} ├─".format(door[3], qt[3],  door[4], qt[4]))
-                print(" 　│　　{}　　{} │　　　　　{} │　　{}　　{} │".format(star[3], qd[3], md, star[4], qd[4]))
-                print(" 　├───────────┼───────────┼───────────┤")
-                print("　 │　　{}　　　 │　　{}　　　 │　　{}　　　 │".format(god[5], god[6], god[7]))
-                print(" {}│　　{}　　{} │　　{}　　{} │　　{}　　{} │{}".format(e_to_s.get("卯"),door[5], qt[5], door[6], qt[6], door[7], qt[7], e_to_s.get("戌")))
-                print(" {}│　　{}　　{} │　　{}　　{} │　　{}　　{} │{}".format(e_to_g.get("卯"),star[5], qd[5], star[6], qd[6], star[7], qd[7], e_to_g.get("戌")))
-                print("  ／────────┬──┴─────┬─────┴──┬────────＼")
-                print("／  {}{}  　 │  {}{}　 │  {}{}　 │  　 {}{}　 ＼".format(e_to_s.get("寅"),e_to_g.get("寅"),e_to_s.get("丑"),e_to_g.get("丑"),e_to_s.get("子"),e_to_g.get("子"),e_to_s.get("亥"),e_to_g.get("亥")))
-                expander = st.expander("原始碼")
-                expander.write(str(qtext))
-            if num == 2:
-                qtext = kinqimen.Qimen(y,m,d,h,mintue).pan_minute(pai)
-                lr = kinliuren.Liuren( qtext.get("節氣"),lunar_month, gz[3], gz[4]).result(0)
-                qd = [qtext.get("地盤").get(i) for i in eg]
-                e_to_s = lr.get("地轉天盤")
-                e_to_g = lr.get("地轉天將")
-                qt = [qtext.get("天盤").get(i) for i in eg]
-                god = [qtext.get("神").get(i) for i in eg]
-                door = [qtext.get("門").get(i) for i in eg]
-                star = [qtext.get("星").get(i) for i in eg]
-                md = qtext.get("地盤").get("中")
-                print("刻家奇門 | {}".format(qtext.get("排盤方式")))
-                print("{}年{}月{}日{}時\n".format(y,m,d,h))
-                print("{} |\n{} | 節氣︰{} |\n值符星宮︰天{}宮 | 值使門宮︰{}\n".format(qtext.get("干支"), qtext.get("排局"),  j_q,  qtext.get("值符值使").get("值符星宮")[0]+"-"+qtext.get("值符值使").get("值符星宮")[1], qtext.get("值符值使").get("值使門宮")[0]+"門"+qtext.get("值符值使").get("值使門宮")[1]+"宮" ))
-                print("＼  {}{}  　 │  {}{}　 │  {}{}　 │  　 {}{}　 ／".format(e_to_s.get("巳"),e_to_g.get("巳"),e_to_s.get("午"),e_to_g.get("午"),e_to_s.get("未"),e_to_g.get("未"),e_to_s.get("申"),e_to_g.get("申")))
-                print("  ＼────────┴──┬─────┴─────┬──┴────────／")
-                print(" 　│　　{}　　　 │　　{}　　　 │　　{}　　　 │".format(god[0], god[1], god[2]))
-                print(" 　│　　{}　　{} │　　{}　　{} │　　{}　　{} │".format(door[0], qt[0], door[1], qt[1], door[2], qt[2]))
-                print(" 　│　　{}　　{} │　　{}　　{} │　　{}　　{} │".format(star[0], qd[0], star[1], qd[1], star[2], qd[2]))
-                print(" {}├───────────┼───────────┼───────────┤{}".format(e_to_s.get("辰"),e_to_s.get("酉")))
-                print(" {}│　　{}　　　 │　　　　　　 │　　{}　　　 │{}".format(e_to_g.get("辰"),god[3], god[4],e_to_g.get("酉")))
-                print("　─┤　　{}　　{} │　　　　　　 │　　{}　　{} ├─".format(door[3], qt[3],  door[4], qt[4]))
-                print(" 　│　　{}　　{} │　　　　　{} │　　{}　　{} │".format(star[3], qd[3], md, star[4], qd[4]))
-                print(" 　├───────────┼───────────┼───────────┤")
-                print("　 │　　{}　　　 │　　{}　　　 │　　{}　　　 │".format(god[5], god[6], god[7]))
-                print(" {}│　　{}　　{} │　　{}　　{} │　　{}　　{} │{}".format(e_to_s.get("卯"),door[5], qt[5], door[6], qt[6], door[7], qt[7], e_to_s.get("戌")))
-                print(" {}│　　{}　　{} │　　{}　　{} │　　{}　　{} │{}".format(e_to_g.get("卯"),star[5], qd[5], star[6], qd[6], star[7], qd[7], e_to_g.get("戌")))
-                print("  ／────────┬──┴─────┬─────┴──┬────────＼")
-                print("／  {}{}  　 │  {}{}　 │  {}{}　 │  　 {}{}　 ＼".format(e_to_s.get("寅"),e_to_g.get("寅"),e_to_s.get("丑"),e_to_g.get("丑"),e_to_s.get("子"),e_to_g.get("子"),e_to_s.get("亥"),e_to_g.get("亥")))
-                expander = st.expander("原始碼")
-                expander.write(str(qtext))
